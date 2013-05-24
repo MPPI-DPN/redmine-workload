731d52fb808ab2d878604fbe25e9f6960040733a
Vincent Agnano <vincent.agnano@scopyleft.fr>
2013-05-24 10:39:57 +0200

    displays list of users filtered

--- app/controllers/workboard_controller.rb
+++ app/controllers/workboard_controller.rb
@@ -1,6 +1,25 @@
 class WorkboardController < ApplicationController
   unloadable
+  menu_item :workload
+
+
+  helper :issues
+  helper :projects
+  helper :queries
+  include QueriesHelper
 
   def index
+    retrieve_query
+
+    @project = Project.find(params[:project_id])
+    issues = Issue.open(true).where("project_id = ?", @project)
+    members = {}
+    for issue in issues
+      member = issue.assigned_to
+      members[member.id] = {:member => member, :issues => []}
+      members[member.id][:issues].push(issue)
+    end
+
+    @member_issues = members
   end
 end
